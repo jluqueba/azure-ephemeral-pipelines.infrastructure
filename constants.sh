5c776ab9d2996ef8be99bec964face46a85acfbc
x284783 <x284783@es.produban.com>
2020-01-10 12:31:42 +0100

    Upload files

---
+++ constants.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# General
+resourceGroup=talksandpocs
+location=westeurope
+
+# VNet, SubNet and NSG
+vNetResourceGroup=talksandpocs
+vnetName=vnetworkloadsample
+agentsSubNetName=snetagents
+nsgName=nsgvnetworkloadsample
+
+# Storage Account
+accountName=staworkloadsample
+containerName=netcoreconfbcn2020
