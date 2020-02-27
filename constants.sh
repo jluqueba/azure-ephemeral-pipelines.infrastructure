45d278f63855995de6817a182543162dd775d6db
x284783 <x284783@es.produban.com>
2020-02-27 12:48:33 +0100

    Added constants and ACR support

--- constants.sh
+++ constants.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # General
-resourceGroup=netcoreconfbcn2020
+resourceGroup=netcoreconf2020
 location=westeurope
 
+# ACR
+acrName=acragentimagepurpose
+
 # VNet, SubNet and NSG
-vNetResourceGroup=netcoreconfbcn2020
+vNetResourceGroup=$resourceGroup
 vnetName=vnetgeneralpurpose
 agentsSubNetName=snetagents
 nsgName=nsgvnetgeneralpurpose
 
 # Storage Account
 accountName=stageneralpurpose
-containerName=cnttestfiles
+containerName=cnttestfilespurpose
